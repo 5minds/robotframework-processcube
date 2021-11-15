a92eb92debe84f1b423f274d6d11edefb42f712f
Martin Möllenbeck <moellenbeck@mm-pro13.office.5minds.de>
2021-11-15 11:47:14 +0100

    Remove unused comments

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-#PYTHONPATH=. robot --loglevel=DEBUG --outputdir=examples examples/*.robot
-
-PYTHONPATH=. python -m robot --name "run tests" --outputdir=tests tests/0*.robot
-
-#open examples/report.html
+PYTHONPATH=. python -m robot --name "run tests" --outputdir=tests tests/*.robot
